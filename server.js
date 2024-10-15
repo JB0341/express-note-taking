f0ffe11bd661e9b9f0ea93202b270546127a853b
j b <j.is.brown@gmail.com>
2024-10-15 12:20:20 -0600

    file restructure to get out of develop folder IOT to get app working, deployment next

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { error } = require('console');
 const uniqid = require('uniqid');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,30 +10,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-app.get('*', (req,res) => 
-res.sendFile(path.join(_dirname, '/public/index.html')));
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         res.send(data);
     })
-});
-
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(_dirname, '/public/notes.html')));
+})
 
 app.post('/api/notes', (req, res) => {
-    fs.readFile('/db/db.json', 'utf-8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         const notes = JSON.parse(data)
         const newNote = req.body
         newNote.id = uniqid()
         notes.push(newNote)
-        fs.writeFile('./db./db.json', JSON.stringify(notes), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
             res.json(newNote)
         })
     })
-});
+})
 
 app.delete('/api/notes/:id', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
@@ -43,8 +40,12 @@ app.delete('/api/notes/:id', (req, res) => {
             res.send(200)
         })
     })
-});
+})
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
-console.log(`Listening on ${PORT}`));
-
+    console.log(`App listening at http://localhost:${PORT}`)
+);
